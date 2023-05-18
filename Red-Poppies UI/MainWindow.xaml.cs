da74f066ce21a5a81d54b6c6ab4528ec4b3ea05f
51xel <20rud15@gmail.com>
2023-05-18 13:08:13 +0300

    Added gitignore, gitattributes. Changed folder structure

---
+++ Red-Poppies UI/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Red_Poppies_UI.Utils;
+using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Red_Poppies_UI {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            WinMax.DoSourceInitialized(this);
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e) {
+            if (WindowState == WindowState.Maximized) {
+                Uri uri = new Uri("/Images/Icons/restore.png", UriKind.Relative);
+                ImageSource imgSource = new BitmapImage(uri);
+                TitlebarButtons.MaximizeButtonImage.Source = imgSource;
+            }
+            else if (WindowState == WindowState.Normal) {
+                Uri uri = new Uri("/Images/Icons/maximize.png", UriKind.Relative);
+                ImageSource imgSource = new BitmapImage(uri);
+                TitlebarButtons.MaximizeButtonImage.Source = imgSource;
+            }
+        }
+    }
+}
