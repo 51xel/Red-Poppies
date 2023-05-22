15d51d90e0c74a54aa15fba0a760c6f4f6e4265b
51xel <20rud15@gmail.com>
2023-05-22 19:47:31 +0300

    Added ClientLog, WorkerLog

---
+++ Red-Poppies UI/View/UserControls/WorkerLog.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Red_Poppies_UI.View.UserControls
+{
+    public partial class WorkerLog : UserControl
+    {
+        public WorkerLog() {
+            InitializeComponent();
+
+            var test = (Style)TextBoxName.FindResource("CustomTextBox");
+            var test2 = test.Setters;
+        }
+    }
+}
