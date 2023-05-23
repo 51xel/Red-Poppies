e1a174f36322c9bf5a5e58edc5792cd29aa62e56
51xel <20rud15@gmail.com>
2023-05-23 13:21:49 +0300

    Added ClientWorkDesk

---
+++ Red-Poppies UI/View/UserControls/ClientWorkDesk.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Red_Poppies_UI.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ClientWorkDesk.xaml
+    /// </summary>
+    public partial class ClientWorkDesk : UserControl
+    {
+        struct Person
+        {
+            public string Name { get; set; }
+            public string NumberOfRoom { get; set; }
+            public string Type { get; set; }
+            public string Pay { get; set; }
+            public string Days { get; set; }
+            public string Date { get; set; }
+        }
+        public ClientWorkDesk()
+        {
+            InitializeComponent();
+
+            List.Items.Add(new Person { Name = "Anton", NumberOfRoom = "1", Type = "Luxe", Pay = "123", Days="14", Date="12.12.12"});
+        }
+    }
+}
